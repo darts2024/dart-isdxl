fa6a06ed5d3d10df9643b35732fbd9980dd05c26
Nasfame <laciferin2024@protonmail.com>
2024-11-24 05:04:21 +0530

    Just: use StableDiffusion

--- download.py
+++ download.py
@@ -1,12 +1,11 @@
-from diffusers import  DiffusionPipeline
+from diffusers import StableDiffusionPipeline
 import torch
 
-model_name = "runwayml/stable-diffusion-v1-5"
-# pipeline = StableDiffusionPipeline.from_pretrained(model_name,
-#
-#                                                    )
+model_name:str = "runwayml/stable-diffusion-v1-5"
+pipeline = StableDiffusionPipeline.from_pretrained(model_name)
 
-DiffusionPipeline.from_pretrained(model_name)
+
+# DiffusionPipeline.from_pretrained(model_name)
 
 
 # pipeline.save_pretrained("./stable-diffusion-v1-5")
