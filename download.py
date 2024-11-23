36752b99f32a929ad5aaaab2acd7997207323f7c
Nasfame <laciferin2024@protonmail.com>
2024-11-24 02:34:09 +0530

    download: diffusion pipeline

--- download.py
+++ download.py
@@ -1,8 +1,10 @@
-from diffusers import StableDiffusionPipeline
-
+from diffusers import StableDiffusionPipeline, DiffusionPipeline
+import torch
 
 model_name = "runwayml/stable-diffusion-v1-5"
-pipeline = StableDiffusionPipeline.from_pretrained(model_name,
-                                                 # torch_dtype=torch.float16, use_safetensors=True, variant="fp16"
-                                                   )
+# pipeline = StableDiffusionPipeline.from_pretrained(model_name,
+#                                                  # torch_dtype=torch.float16, use_safetensors=True, variant="fp16"
+#                                                    )
+
+DiffusionPipeline.from_pretrained(model_name, torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
 # pipeline.save_pretrained("./stable-diffusion-v1-5")
