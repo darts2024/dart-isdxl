5c2677207b51d8b5a453b9b36e22674268a5946f
Nasfame <laciferin2024@protonmail.com>
2024-11-24 06:49:05 +0530

    download: fixed

--- download.py
+++ download.py
@@ -4,10 +4,10 @@ model_name:str = "runwayml/stable-diffusion-v1-5"
 
 try:
   from diffusers import StableDiffusionPipeline
-  pipeline = StableDiffusionPipeline.from_pretrained(model_name)
+  pipeline = StableDiffusionPipeline.from_pretrained(model_name,revision="fp16", torch_dtype=torch.float16)
 except:
   from diffusers import DiffusionPipeline
-  DiffusionPipeline.from_pretrained(model_name)
+  DiffusionPipeline.from_pretrained(model_name,revision="fp16",torch_dtype=torch.float16)
 
 
 # pipeline.save_pretrained("./stable-diffusion-v1-5")
