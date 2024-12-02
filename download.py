5213cd93ddcce44e1be930bd5594e58d7bc07145
Nasfame <laciferin2024@protonmail.com>
2024-12-02 18:36:26 +0530

    download: prioritize diffusion pipeline

--- download.py
+++ download.py
@@ -3,11 +3,11 @@ import torch
 from config import model_name
 
 try:
-  from diffusers import StableDiffusionPipeline
-  pipeline = StableDiffusionPipeline.from_pretrained(model_name)
-except:
   from diffusers import DiffusionPipeline
   DiffusionPipeline.from_pretrained(model_name)
+except:
+  from diffusers import StableDiffusionPipeline
+  pipeline = StableDiffusionPipeline.from_pretrained(model_name)
 
 
 # pipeline.save_pretrained("./stable-diffusion-v1-5")
