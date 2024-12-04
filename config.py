592fd2e2f9758dc47a7faea2ac7fb8ca477c0597
Nasfame <laciferin2024@protonmail.com>
2024-12-04 16:41:52 +0530

    fix: type issue model_path to str

--- config.py
+++ config.py
@@ -2,16 +2,17 @@
 from pathlib import Path
 from typing import Final
 
+import os
 
 model_name:Final[str] = "runwayml/stable-diffusion-v1-5"
 model_name:Final[str] = "stabilityai/stable-diffusion-xl-base-1.0"
 
-base_model_dir = Path("./models")
+base_model_dir = "./models"
 
 
-def get_model_path(model_name: str):
+def get_model_path(model_name: str)->str:
     # Create target directory based on model name
-    model_dir = base_model_dir / model_name.replace("/", "_")
+    model_dir = os.path.join(base_model_dir,model_name.replace("/", "_"))
     
     return model_dir
   
