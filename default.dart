9ddc6ba3b52fd65cae58e8cb3d7db636b24111d3
Nasfame <laciferin2024@protonmail.com>
2024-12-26 08:56:15 +0530

    isdxl: docker tag v1.5.0-docker

--- default.dart
+++ default.dart
@@ -15,7 +15,7 @@
                     "bash", "-c",
                     "python3 /app/inference.py"
                 ],
-                "Image": "ghcr.io/darts2024/isdxl:{{ or .dockerTag "v1.5.0"}}",
+                "Image": "ghcr.io/darts2024/isdxl:{{ or .dockerTag "v1.5.0-docker"}}",
                 "EnvironmentVariables": [
                     {{if .Prompt}}"{{ subt "PROMPT=%s" .Prompt }}"{{else}}"PROMPT=cute rabbit in a spacesuit"{{end}},
                     "{{ subt "RANDOM_SEED=%s" (or .Seed "1")  }}",
